84803f8abbf9b78f1570a579c43d092e666f9fe4
mrmalvi <malviyak00@gmail.com>
2025-09-06 15:16:25 +0530

    first commit

---
+++ lib/rails_error_notifier.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rails_error_notifier/version"
+
+module RailsErrorNotifier
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rails_error_notifier/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RailsErrorNotifier
+  VERSION = "0.1.0"
+end
